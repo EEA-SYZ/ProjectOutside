a14a0346b353e421b8fe92761c2b270982592164
EEA-SYZ <eea_syz@126.com>
2025-10-27 11:28:28 +0800

    init

---
+++ src/config/global-config.ts
@@ -0,0 +1,24 @@
+export const app_title = '智能心理健康管理系统';
+
+// 登录页面
+export const loginConfig = {
+    // 背景图片
+    background_image: 'assets/imgs/login_bk.png',
+    // 登录表单标题
+    form_title: '智能心理健康管理系统'
+};
+
+// menu配置
+export const menuConfig = {
+    // 菜单位置，可选值为 'left'、'top'
+    placement: 'left'
+};
+
+// 版权配置
+export const copyright = '智能心理健康管理系统 ©2025 Implement By ***'; // EEA TODO
+
+// session配置
+export const sessionConfig = {
+    // session在浏览器的storerage中的存储key
+    store_key: '__my-user-session__' // EEA TO_MAKE_CLEAR
+};
